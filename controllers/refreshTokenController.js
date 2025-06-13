6532326d70b73c45054e1dd660580f6164714b04
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2025-06-13 10:54:12 +0530

    Set up MongoDB for auth actions

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,27 +1,20 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+const User = require("../model/User");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const handleTokenRefresh = (req, res) => {
+const handleTokenRefresh = async (req, res) => {
   const cookies = req.cookies;
 
   if (!cookies?.jwt) return res.sendStatus(401);
   const refreshToken = cookies.jwt;
 
-  const foundUser = usersDB.users.find(
-    (person) => person.refreshToken === refreshToken
-  );
+  const foundUser = await User.findOne({ refreshToken }).exec();
   if (!foundUser) return res.sendStatus(403);
 
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
     if (err || foundUser.username !== decoded.username)
       return res.sendStatus(403);
-
+    const roles = Object.values(foundUser.roles);
     const accessToken = jwt.sign(
       {
         "UserInfo": {
@@ -32,7 +25,7 @@ const handleTokenRefresh = (req, res) => {
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "5m" }
     );
-    res.json({ accessToken });
+    res.json({ roles, accessToken });
   });
 };
 
